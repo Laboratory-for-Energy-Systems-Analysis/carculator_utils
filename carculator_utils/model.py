Fix calculation with static background

carculator_utils/model.py
@@ -408,9 +408,10 @@ class VehicleModel:
                     ).T
 
             # we flag vehicles that are not compliant
-            self["TtW energy"] = np.where(
-                (self["driving mass"] < self["gross mass"]), 0, self["TtW energy"]
-            )
+            if "gross mass" in self.array.parameter.values:
+                self["TtW energy"] = np.where(
+                    (self["driving mass"] > self["gross mass"]), 0, self["TtW energy"]
+                )
 
     def calculate_ttw_energy(self) -> None:
         """