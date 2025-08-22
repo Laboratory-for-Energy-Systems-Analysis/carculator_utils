2a3f4ce8334539f83a2003d43596719003bf6dfe
romainsacchi <r_s@me.com>
2025-08-22 15:38:00 +0000

    Black reformating

--- carculator_utils/model.py
+++ carculator_utils/model.py
@@ -1026,7 +1026,6 @@ class VehicleModel:
                     dim="parameter"
                 )
 
-
     def override_range(self):
         """
         Set storage size or range for each powertrain.
